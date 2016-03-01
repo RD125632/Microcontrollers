6f30a1317db4aa1d8f7afe51e8af30356f79868c
RD125632 <titat1993@gmail.com>
2016-03-01 15:01:10 +0100

    TIB3

--- Track4/TI4B3/main.c
+++ Track4/TI4B3/main.c
@@ -1,18 +1,36 @@
 /*
- * TI4B3.c
+ * B3.1.c
  *
- * Created: 1-3-2016 13:59:17
- * Author : titat_000
+ * Created: 1-3-2016 13:24:17
+ * Author : aares
  */ 
 
 #include <avr/io.h>
+#include <avr/delay.h>
 
+void wait(int ms)
+{
+	for(int i = 0; i < ms; i++)
+	{
+		_delay_ms(1);
+	}
+}
 
 int main(void)
 {
+	ADCSRA = 0b111000001;
+	ADMUX = 0b11000001;
+	DDRA = 0xFF;
+	DDRB = 0xFF;
+	DDRF = 0x00;
+
     /* Replace with your application code */
     while (1) 
     {
+		ADCSRA = 0b111000001;
+		PORTA = ADCL;
+		PORTB = ADCH;
+		wait(100);
     }
 }
 
